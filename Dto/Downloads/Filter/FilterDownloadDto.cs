b4f9238226191290eff894df7f17706ee7bee00c
EduardoProfe <eduardoglez64377@gmail.com>
2024-02-04 13:18:34 -0500

    Update FilterDownloadDto.cs

--- Dto/Downloads/Filter/FilterDownloadDto.cs
+++ Dto/Downloads/Filter/FilterDownloadDto.cs
@@ -8,6 +8,8 @@ public class FilterDownloadDto
     public required Format? Format { get; set; }
     public required DateTimeOffset? DateDownload { get; set; }
     public required string? Username { get; set; }
+    public required int? DocumentId { get; set; }
+    public required int? ReasonId { get; set; }
 
     public PaginationParams PaginationParams { get; set; } = new PaginationParams();
     public SortByDownload SortBy { get; set; } = SortByDownload.Id;
